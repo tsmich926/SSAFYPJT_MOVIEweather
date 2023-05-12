fdfd7b7179546d385a1c76bc020971cfa7677f18
황수아 <tsmich926@naver.com>
2023-05-12 17:03:37 +0900

    Update RandomView.vue

--- src/views/RandomView.vue
+++ src/views/RandomView.vue
@@ -1,22 +1,21 @@
-
 <template>
   <div>
     <h2>Random Movie</h2>
     <div v-if="weather">
-      <h3>날씨: {{ weather }}</h3>
+      <h3>오늘의 날씨 {{ weather }}에 맞는 영화 추천</h3>
     </div>
 
     <button @click="getRandomMovie">랜덤</button>
-    <div v-if="randomMovie">
-      <h3>{{ randomMovie.title }}</h3>
-      <img
-        :src="'https://image.tmdb.org/t/p/w200/' + randomMovie.poster_path"
-        :alt="randomMovie.title"
-      />
-    </div>
-  </div>
-</template>
+<div v-if="randomMovie">
+  <h3>{{ randomMovie.genres_name }}</h3>
+  <img
+    :src="'https://image.tmdb.org/t/p/w200/' + randomMovie.poster_path"
+    :alt="randomMovie.title"
+  />
+</div>
 
+ </div>
+</template>
 <script>
 import axios from "axios";
 
@@ -33,18 +32,21 @@ export default {
     axios
       .get("https://api.themoviedb.org/3/movie/top_rated?language=ko-KR", {
         params: {
-          api_key: "6e4f60ea88e44702b1782875498a4af2", // The Movie Database API 키로 대체해야 합니다.
+          api_key: "6e4f60ea88e44702b1782875498a4af2", 
         },
       })
       .then((response) => {
         this.movies = response.data.results;
         this.randomMovie = this.movies[0];
+
       })
       .catch((error) => {
         console.log(error);
       });
-    this.getWeather();
+
+      this.getWeather();
   },
+  
   methods: {
     getRandomMovie() {
       const randomIndex = Math.floor(Math.random() * this.movies.length);
@@ -54,19 +56,37 @@ export default {
       axios
         .get("https://api.openweathermap.org/data/2.5/weather?q=Seoul,kor", {
           params: {
-            APPID: "bf43c0b6dd7428e9ad104aed9e61da4d", // OpenWeatherMap API 키로 대체해야 합니다.
+            APPID: "bf43c0b6dd7428e9ad104aed9e61da4d",
           },
         })
         .then((response) => {
-          this.weather = response.data.weather[0].main;
+          const weather = response.data.weather[0].main;
+          this.recommendMovieBasedOnWeather(weather);
+          this.weather = weather;
         })
         .catch((error) => {
           console.log(error);
         });
     },
+    recommendMovieBasedOnWeather(weather) {
+      if (weather === "Clear") {
+        this.randomMovie = this.movies.find(
+          (movie) => movie.genre_ids.includes(80) //드라마
+        );
+      
+      } else if (weather === "Clouds") {
+        this.randomMovie = this.movies.find(
+          (movie) =>movie.genre_ids.includes(18) //범죄
+        );
+
+      } else {
+        this.randomMovie = this.movies.find(
+          (movie) => movie.genre_ids.includes(14) //판타지
+        );
+      }
+    },
   },
 };
 </script>
-
 <style>
 </style>
